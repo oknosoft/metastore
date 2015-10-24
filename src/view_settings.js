52284308e3f9b9597eb83118e4538834f483e43c
unpete <info@oknosoft.ru>
2015-10-25 02:03:39 +0500

    v0.0.1

--- src/view_settings.js
+++ src/view_settings.js
@@ -12,6 +12,23 @@ $p.iface.set_view_settings = function (cell) {
 	if($p.iface._settings)
 		return;
 
-	cell.attachHTMLString("<div>Настройки не заданы</div>");
+	$p.iface._settings = {
+		form: cell.attachForm([
+			{ type:"fieldset" , name:"device_type", label:"Тип устройства", labelAlign:"left", list:[
+
+				{ type:"block" , name:"form_block_2", list:[
+					{ type:"settings" , labelWidth:80, labelAlign:"left", position:"label-right"  },
+					{ type:"radio" , name:"device_type_form_radio", label:"Компьютер", value:"desktop"},
+					{ type:"newcolumn"   },
+					{ type:"radio" , name:"device_type_form_radio", label:"Телефон", value:"phone"},
+					{ type:"newcolumn"   },
+					{ type:"radio" , name:"device_type_form_radio", label:"Планшет", value:"tablet"}
+				]  },
+
+				{ type:"template" , name:"form_template_1", label:"<span style='color: #888'>Класс устройства определяется автоматически, но пользователь может задать его явно</span>" }
+			]  }
+		])
+	};
+
 
 };
